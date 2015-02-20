fe0f37af1201a99fb7840e475d9fbb8757b5ab83
Ayush Goel <ayush.g@directi.com>
2015-02-20 14:39:23 +0530

    Change wallpaper randonly in VC

--- Wily-ios/Wily/ViewController.m
+++ Wily-ios/Wily/ViewController.m
@@ -2,12 +2,21 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *wallPaperImageView;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
   [super viewDidLoad];
+  [self changeWallPaper];
+}
+
+- (void)changeWallPaper {
+  NSUInteger wallPaperNumber = arc4random() % 37;
+  NSString *wallPaperImageName = [NSString stringWithFormat:@"%@", @(wallPaperNumber)];
+  self.wallPaperImageView.image = [UIImage imageNamed:wallPaperImageName];
 }
 
 @end
