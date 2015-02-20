e9ca9ff657cf8bd40bd7c6ef48c23bc5996b90f3
Isneesh Marwah <isneesh.m@directi.com>
2015-02-21 00:07:58 +0530

    replace MediaPlayer with ExoPlayer

--- android/app/src/main/java/com/dashdash/wily/PlayerActivity.java
+++ android/app/src/main/java/com/dashdash/wily/PlayerActivity.java
@@ -2,6 +2,7 @@ package com.dashdash.wily;
 
 import android.app.Activity;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,12 +11,17 @@ import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import com.dashdash.wily.playlist.Playlist;
+import com.google.android.exoplayer.ExoPlayer;
+import com.google.android.exoplayer.FrameworkSampleSource;
+import com.google.android.exoplayer.MediaCodecAudioTrackRenderer;
+import com.google.android.exoplayer.util.PlayerControl;
 
 
 public class PlayerActivity extends Activity {
 
     Playlist _playlist;
-    private MediaPlayer mediaPlayer;
+    private ExoPlayer player;
+    private PlayerControl playerControl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +38,19 @@ public class PlayerActivity extends Activity {
         final View root = findViewById(R.id.root);
         final ImageView wallpaper = (ImageView) findViewById(R.id.wallpaper);
         final TextView songTitle = (TextView) findViewById(R.id.song_title);
-        final Button btnPlayPause = (Button) findViewById(R.id.btn_play);
 
 
         Playlist.Entry songEntry = _playlist.getNext();
         songTitle.setText(songEntry.name);
 
-        mediaPlayer = MediaPlayer.create(this, songEntry.uri);
-        mediaPlayer.start();
+        player = ExoPlayer.Factory.newInstance(1);
+        FrameworkSampleSource frameworkSampleSource = new FrameworkSampleSource(this, songEntry.uri, null, 1);
+        MediaCodecAudioTrackRenderer mediaCodecAudioTrackRenderer = new MediaCodecAudioTrackRenderer(frameworkSampleSource);
+        player.prepare(mediaCodecAudioTrackRenderer);
+        player.setPlayWhenReady(true);
+
+        playerControl = new PlayerControl(player);
+
     }
 
     @Override
@@ -51,9 +62,9 @@ public class PlayerActivity extends Activity {
 
         boolean on = ((ToggleButton) view).isChecked();
         if (on) {
-            mediaPlayer.pause();
+            playerControl.pause();
         } else {
-            mediaPlayer.start();
+            playerControl.start();
         }
     }
 }
