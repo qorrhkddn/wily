998943a5eac734312cf9bf05aa7f26ea5ff0c136
Manav Rathi <mx4492@gmail.com>
2015-02-20 15:16:11 +0530

    Adapt minimal XCDYouTubeKit example to an invisible player

--- Wily-ios/Wily/ViewController.m
+++ Wily-ios/Wily/ViewController.m
@@ -1,13 +1,23 @@
 #import "ViewController.h"
+#import "InvisibleYouTubeVideoPlayer.h"
+static NSString * const VideoIdentifier = @"vrfAQI-TIVM";
 
 @interface ViewController ()
-
+@property (nonatomic) InvisibleYouTubeVideoPlayer *player;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
   [super viewDidLoad];
+
+  [self performSelector:@selector(playVideo) withObject:nil afterDelay:1];
+}
+
+- (void)playVideo {
+  self.player = [[InvisibleYouTubeVideoPlayer alloc] initWithContainerView:self.view
+                                                           videoIdentifier:VideoIdentifier];
+  [self.player play];
 }
 
 @end
