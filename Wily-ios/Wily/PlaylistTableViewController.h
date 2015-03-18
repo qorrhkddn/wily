bf571f983a2454c1befb5246cb56456126c894e3
Manav Rathi <mx4492@gmail.com>
2015-03-18 19:06:06 +0530

    Add table view container for listing offline songs

---
+++ Wily-ios/Wily/PlaylistTableViewController.h
@@ -0,0 +1,5 @@
+@import UIKit;
+
+@interface PlaylistTableViewController : UITableViewController
+
+@end
---
+++ Wily-ios/Wily/PlaylistTableViewController.m
@@ -0,0 +1,61 @@
+#import "PlaylistTableViewController.h"
+
+@interface PlaylistTableViewController ()
+
+@end
+
+@implementation PlaylistTableViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+
+  // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+  // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+#pragma mark Table View Data Source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+  return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+  return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PlaylistTableViewCellReuseIdentifier"
+                                                          forIndexPath:indexPath];
+
+  return cell;
+}
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
